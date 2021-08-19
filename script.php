623b07719fbc77c830620b7c330eb9c1513a1a97
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-08-19 15:02:43 +0200

    Script is saving xml data to json file

--- script.php
+++ script.php
@@ -1,15 +1,16 @@
 <?php
-    $xmlFromWebsite = file_get_contents("https://dlabystrzakow.pl/xml/produkty-dlabystrzakow.xml");
-    $data = simplexml_load_string($xmlFromWebsite);
-    
-    foreach ($data->lista->ksiazka as $ksiazka)
-    {
-        echo "Ident: " . $ksiazka->ident . "\n";
-        echo "Tytuł: " . $ksiazka->tytul . "\n";
-        echo "Liczba stron: " . $ksiazka->liczbastron . "\n";
-        echo "Data wydania: " . $ksiazka->datawydania . "\n" . "\n";
-    }
+$xmlFromWebsite = file_get_contents("https://dlabystrzakow.pl/xml/produkty-dlabystrzakow.xml");
+$dataFromWebsite = simplexml_load_string($xmlFromWebsite);
+file_put_contents('file.json', '{', FILE_APPEND);
+foreach ($dataFromWebsite->lista->ksiazka as $ksiazka) {
+    $singleData = [
+        '"ident"' . ':' . '"' . $ksiazka->ident . '"' . ',' . "\n",
+        '"tytul"' . ':' . '"' . $ksiazka->tytul . '"' . ',' ."\n",
+        '"liczbaStron"' . ':' . $ksiazka->liczbastron . ',' ."\n",
+        '"dataWydania"' . ':' . '"' . $ksiazka->datawydania . '"' . ',' ."\n",
+    ];
+    file_put_contents('file.json', $singleData, FILE_APPEND);
+}
 
-    
+file_put_contents('file.json', '}', FILE_APPEND);
 ?>
-
