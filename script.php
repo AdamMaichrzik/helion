5ebb1a8e8d5e9cd201a8c3a8c002df3aea75eae0
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-08-20 11:56:54 +0200

    Correct json file syntax. Script is working

--- script.php
+++ script.php
@@ -1,16 +1,16 @@
 <?php
 $xmlFromWebsite = file_get_contents("https://dlabystrzakow.pl/xml/produkty-dlabystrzakow.xml");
 $dataFromWebsite = simplexml_load_string($xmlFromWebsite);
-file_put_contents('file.json', '{', FILE_APPEND);
+file_put_contents('file.json', '[', FILE_APPEND);
 foreach ($dataFromWebsite->lista->ksiazka as $ksiazka) {
     $singleData = [
-        '"ident"' . ':' . '"' . $ksiazka->ident . '"' . ',' . "\n",
+        '{' . '"ident"' . ':' . '"' . $ksiazka->ident . '"' . ',' . "\n",
         '"tytul"' . ':' . '"' . $ksiazka->tytul . '"' . ',' ."\n",
-        '"liczbaStron"' . ':' . $ksiazka->liczbastron . ',' ."\n",
-        '"dataWydania"' . ':' . '"' . $ksiazka->datawydania . '"' . ',' ."\n",
+        '"liczbaStron"' . ':' . '"' . $ksiazka->liczbastron . '"' . ',' ."\n",
+        '"dataWydania"' . ':' . '"' . $ksiazka->datawydania . '"' . '}' . ',' ."\n",
     ];
     file_put_contents('file.json', $singleData, FILE_APPEND);
 }
 
-file_put_contents('file.json', '}', FILE_APPEND);
+file_put_contents('file.json', ']', FILE_APPEND);
 ?>
