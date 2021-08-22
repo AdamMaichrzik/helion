cab541ac85b905395f9264f59cfcfa68b3eb1cc4
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-08-22 22:03:45 +0200

    New json file name

--- script.php
+++ script.php
@@ -1,16 +1,16 @@
 <?php
-$xmlFromWebsite = file_get_contents("https://dlabystrzakow.pl/xml/produkty-dlabystrzakow.xml");
-$dataFromWebsite = simplexml_load_string($xmlFromWebsite);
-file_put_contents('file.json', '[', FILE_APPEND);
-foreach ($dataFromWebsite->lista->ksiazka as $ksiazka) {
-    $singleData = [
-        '{' . '"ident"' . ':' . '"' . $ksiazka->ident . '"' . ',' . "\n",
-        '"tytul"' . ':' . '"' . $ksiazka->tytul . '"' . ',' ."\n",
-        '"liczbaStron"' . ':' . '"' . $ksiazka->liczbastron . '"' . ',' ."\n",
-        '"dataWydania"' . ':' . '"' . $ksiazka->datawydania . '"' . '}' . ',' ."\n",
-    ];
-    file_put_contents('file.json', $singleData, FILE_APPEND);
-}
+    $xmlFromWebsite = file_get_contents("https://dlabystrzakow.pl/xml/produkty-dlabystrzakow.xml");
+    $dataFromWebsite = simplexml_load_string($xmlFromWebsite);
+    file_put_contents('JSONTableFromXML.json', '[', FILE_APPEND);
+    foreach ($dataFromWebsite->lista->ksiazka as $ksiazka) {
+        $singleData = [
+            '{' . '"ident"' . ':' . '"' . $ksiazka->ident . '"' . ',' . "\n",
+            '"tytul"' . ':' . '"' . $ksiazka->tytul . '"' . ',' ."\n",
+            '"liczbaStron"' . ':' . '"' . $ksiazka->liczbastron . '"' . ',' ."\n",
+            '"dataWydania"' . ':' . '"' . $ksiazka->datawydania . '"' . '}' . ',' ."\n",
+        ];
+        file_put_contents('JSONTableFromXML.json', $singleData, FILE_APPEND);
+    }
 
-file_put_contents('file.json', ']', FILE_APPEND);
+    file_put_contents('JSONTableFromXML.json', ']', FILE_APPEND);
 ?>
