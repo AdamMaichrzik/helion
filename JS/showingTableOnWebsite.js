eb6bb5b062f4726b5cd8e80f88c1a25f512904db
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-08-22 22:58:42 +0200

    Code look changes

--- JS/showingTableOnWebsite.js
+++ JS/showingTableOnWebsite.js
@@ -1,14 +1,14 @@
 function showTable() {
-    $.getJSON("../JSON/JSONTableFromXML.json", function (data) {
-        JSONItems = data;
-        var tr;
-        for (var i = 0; i < JSONItems.length; i++) {
-            tr = $("<tr/>");
-            tr.append("<td>" + data[i].ident + "</td>");
-            tr.append("<td>" + data[i].tytul + "</td>");
-            tr.append("<td>" + data[i].liczbaStron + "</td>");
-            tr.append("<td>" + data[i].dataWydania + "</td>");
-            $("table").append(tr);
-        }
-    });
+	$.getJSON("../JSON/JSONTableFromXML.json", function (data) {
+		JSONItems = data;
+		var tr;
+		for (var i = 0; i < JSONItems.length; i++) {
+			tr = $("<tr/>");
+			tr.append("<td>" + data[i].ident + "</td>");
+			tr.append("<td>" + data[i].tytul + "</td>");
+			tr.append("<td>" + data[i].liczbaStron + "</td>");
+			tr.append("<td>" + data[i].dataWydania + "</td>");
+			$("table").append(tr);
+		}
+	});
 }
