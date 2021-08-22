75b38478fb14b58cfc120bfef55a224e3a1847ae
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-08-22 22:10:16 +0200

    Some comments

--- script.php
+++ script.php
@@ -1,7 +1,9 @@
 <?php
+    //Making php variable from xml file on the website
     $xmlFromWebsite = file_get_contents("https://dlabystrzakow.pl/xml/produkty-dlabystrzakow.xml");
     $dataFromWebsite = simplexml_load_string($xmlFromWebsite);
     file_put_contents('JSONTableFromXML.json', '[', FILE_APPEND);
+    //Loop for putting the books to the JSON file
     foreach ($dataFromWebsite->lista->ksiazka as $ksiazka) {
         $singleData = [
             '{' . '"ident"' . ':' . '"' . $ksiazka->ident . '"' . ',' . "\n",
